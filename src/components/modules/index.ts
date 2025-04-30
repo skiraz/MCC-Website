7d6b45446d6958fac02518f0d3679d2d695f6635
saif zaidn <ewiwisaif5@gmail.com>
2025-04-30 21:59:44 +0300

    hello :x

--- src/components/modules/index.ts
+++ src/components/modules/index.ts
@@ -1,7 +1,7 @@
 // src/components/modules/index.ts
 import Navbar from "./Navbar/Navbar.astro"
 import About from "./About/About.astro"
-import Services from "./Sevices/Services.astro"
+import Services from "./Services/Services.astro"
 import Courses from "./Courses/Courses.astro"
 import Portfolio from "./Portfolio/Portfolio.astro"
 import Products from "./Products/Products.astro"
